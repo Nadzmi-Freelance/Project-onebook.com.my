db4be8828115b8e78c6b77e1e34c0f4fb189f0d7
Nadzmi-Idzham <nadzmiidzham@gmail.com>
2016-08-29 23:47:49 +0800

    First Commit
    A mobile website for project onebook resepi

---
+++ v1/js/header.js
@@ -0,0 +1,6 @@
+function showDropDown() {
+    if(document.getElementById("nav-dropdown-content").style.display == "none")
+      document.getElementById("nav-dropdown-content").style.display = "block";
+    else
+      document.getElementById("nav-dropdown-content").style.display = "none";
+}
